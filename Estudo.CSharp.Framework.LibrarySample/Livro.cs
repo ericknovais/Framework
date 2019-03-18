0474330d4beb0a919071c3e2779c0af42f848f86
Erick Novais <erick.hora@Outook.com>
2019-03-18 07:05:12 -0300

    Primeiro commit para criação e inclusão dos projetos.

---
+++ Estudo.CSharp.Framework.LibrarySample/Livro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo.CSharp.Framework.LibrarySample
+{
+    public class Livro
+    {
+        private string titulo;
+
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+
+        private string autor;
+
+        public string Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }
+
+
+        private int anoPublicacao;
+
+        public int AnoPublicacao
+        {
+            get { return anoPublicacao; }
+            set { anoPublicacao = value; }
+        }
+
+    }
+}
---
+++ Estudo.CSharp.Framework.UIConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Estudo.CSharp.Framework.LibrarySample;
+
+namespace Estudo.CSharp.Framework.UIConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD");
+            Console.ReadKey();
+
+            Livro livro = new Livro();
+           
+        }
+    }
+}
