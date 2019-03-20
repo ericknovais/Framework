3abc3d9fd637456b3d0cd76fbd15c25007d61b53
Erick Novais <erick.hora@Outook.com>
2019-03-20 06:46:32 -0300

    System.Text

---
+++ System.Text_Exemple/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Text_Exemple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region StringBuilder
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("New ");
+            sb.Append("Youk");
+
+            sb.Insert(4, " HEllO ");
+            sb.Remove(4, 6);
+            sb.Replace('k', 'q');
+
+            Console.WriteLine(sb.ToString());
+            sb.Clear();
+            Console.ReadKey();
+            #endregion
+
+
+        }
+    }
+}
