070667a78d3ec11165ae0d78a07cacb854cfc2a1
Erick Novais <erick.hora@Outook.com>
2019-03-21 06:49:38 -0300

    Resource_Exemple

---
+++ System.Resource_Exemple/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Resources;
+
+namespace System.Resource_Exemple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Resources.ResourceManager rm = new ResourceManager(typeof(System.Resource_Exemple.Main));
+            string d = rm.GetString("DESCRICAO");
+            Console.WriteLine(d);
+            // Console.ReadKey();
+
+            Console.WriteLine(ResorceHelper.getResorce("DESCRICAO"));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ System.Resource_Exemple/ResorceHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Resource_Exemple
+{
+    class ResorceHelper
+    {
+        public static string getResorce(string key)
+        {
+            System.Resources.ResourceManager rm = new System.Resources.ResourceManager(typeof(System.Resource_Exemple.Main));
+            return rm.GetString(key);
+        }      
+    }
+}
