71aa2ef79577196e2e22b9dac30ac38c6ede05cf
Erick Novais <erick.hora@Outook.com>
2019-03-26 06:50:46 -0300

    Utilizando a UI como Windows forms, fazendo a criação de uma agenda telefonica usando xml

---
+++ System.XML_Exemple/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace System.XML_Exemple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(15, 64);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 3;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(15, 25);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(15, 90);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Telefone:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtTelefone);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Windows.Forms.TextBox txtTelefone;
+        private Windows.Forms.TextBox txtNome;
+        private Windows.Forms.Button btnSalvar;
+        private Windows.Forms.Label label1;
+        private Windows.Forms.Label label2;
+    }
+}
+
---
+++ System.XML_Exemple/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace System.XML_Exemple
+{
+
+    public partial class Form1 : Form
+    {
+        string arquivo = @"E:\Erick\Estudos\DevMidia C#\Framework\System.XML_Exemple\XML\Agenda.xml";
+        XmlDocument xmlDocument = new XmlDocument();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (!File.Exists(arquivo))
+            {
+                
+                XmlNode nodeRoot = xmlDocument.CreateElement("Contato");
+                xmlDocument.AppendChild(nodeRoot);
+                xmlDocument.Save(arquivo);
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            xmlDocument.Load(arquivo);
+            XmlNode nodeNome = xmlDocument.CreateElement("Nome");
+            XmlNode nodeTelefone = xmlDocument.CreateElement("Telefone");
+            nodeNome.InnerText = txtNome.Text;
+            nodeTelefone.InnerText = txtTelefone.Text;
+            xmlDocument.SelectSingleNode("/Contato").AppendChild(nodeNome);
+            xmlDocument.SelectSingleNode("/Contato").AppendChild(nodeTelefone);
+            xmlDocument.Save(arquivo);
+            LimparCampos();
+        }
+
+        private void LimparCampos()
+        {
+            txtNome.Text = string.Empty;
+            txtTelefone.Text = string.Empty;
+            txtNome.Focus();
+        }
+    }
+}
