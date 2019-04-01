b4b622d1efe4420ed574798ca9451abc5544a293
ericknovais <47401961+ericknovais@users.noreply.github.com>
2019-04-01 14:48:43 -0300

    Ajuste de layout no formAgenda3.
    Com ajustes e incluções de validações

---
+++ SContatos.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+
+namespace System.XML_Exemple
+{
+    public class SContatos
+    {
+        static string arquivo = @"E:\Erick\Estudos\DevMidia C#\Framework\System.XML_Exemple\XML\Agenda.xml";
+        static XmlDocument xDoc = new XmlDocument();
+        static XElement elementX;
+        static Contatos contatos;
+
+        public SContatos()
+        {
+            if (!File.Exists(arquivo))
+            {
+                XmlNode nodeRoot = xDoc.CreateElement("Contatos");
+                xDoc.AppendChild(nodeRoot);
+                xDoc.Save(arquivo);
+            }
+        }
+
+        public static Contatos Read()
+        {
+            //Deserelizador
+            elementX = XElement.Load(arquivo);
+            contatos = Serializador.Deserialize<Contatos>(elementX);
+
+            return contatos;
+        }
+
+        public static void Write(Contatos contatos)
+        {
+            //Serializador
+            XElement xReturn = Serializador.Serialize<Contatos>(contatos);
+            xReturn.Save(arquivo);
+        }
+
+    }
+}
