d6613815e7272ccae9dcba71da57731bcf7a65b3
ericknovais <erick.hora@Outlook.com>
2019-04-15 07:19:06 -0300

    Aula 41 - Criando Conexões e Parâmetros

--- Aplicacao/Program.cs
+++ Aplicacao/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Aplicacao
 {
@@ -11,25 +12,40 @@ namespace Aplicacao
     {
         static void Main(string[] args)
         {
-            string ConnectionString = @"Data Source=DH\DESENV;Initial Catalog=Cadastro;
-                                        User ID=sa;Password=endh";
-                                        //Connect Timeout=30;Encrypt=False;
-                                        //TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            SqlConnection con = new SqlConnection(ConnectionString);
-            con.Open();
-            try
+            //string ConnectionString = @"Server = DH\DESENV; Database = Cadastro;
+            //User ID = sa; Password = endh";
+            //Connect Timeout=30;Encrypt=False;
+            //TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            var conBuilder = getConnectionStringFromConfig();
+
+            var ConnectionString = conBuilder;
+
+            using (var con = new SqlConnection(ConnectionString))
             {
+                Console.WriteLine("String de conexão: " + ConnectionString);
+                con.Open();
                 Console.WriteLine("Conexão com banco de dados efetuada com sucesso...");
                 Console.WriteLine("Estado da conexão " + con.State);
-
-            }
-            finally
-            {
                 con.Close();
                 Console.WriteLine("Estado da conexão " + con.State);
             }
-
             Console.ReadKey();
+        }
+
+        private static string getConnectionStringFromConfig()
+        {
+           return ConfigurationManager.ConnectionStrings["Cadastro"].ConnectionString;
+        }
+
+        private static SqlConnectionStringBuilder getConnectionStringFromBuilder()
+        {
+            var conBuilder = new SqlConnectionStringBuilder();
+            conBuilder.DataSource = @"DH\DESENV";
+            conBuilder.InitialCatalog = "Cadastro";
+            conBuilder.UserID = "sa";
+            conBuilder.Password = "endh";
+            return conBuilder;
         }
     }
 }
