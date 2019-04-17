7492d5dea898ea937cf3803ea3a1948e9193cf38
ericknovais <erick.hora@Outlook.com>
2019-04-17 06:53:03 -0300

    Aula 50 - Criando um Servidor WCF

---
+++ Servico/OlaMundoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Servico
+{
+    public class OlaMundoService : IOlaMundoService
+    {
+        public string OlaMundo()
+        {
+            return "Ola Mundo - WCF";
+        }
+    }
+}
