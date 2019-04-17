38a726ce2b379a21a8b8d8df1bafc01beed54b2d
ericknovais <erick.hora@Outlook.com>
2019-04-16 23:41:39 -0300

    Aula 48 - Persistindo Objetos

--- EF/Program.cs
+++ EF/Program.cs
@@ -10,6 +10,32 @@ namespace EF
     {
         static void Main(string[] args)
         {
+            var ctx = new CadastroEntities();
+            var cli = new CLIENTE()
+            {
+                NomeCliente = "Luan Ferreira",
+                Email = "luan.fereira@gmail.com"
+            };
+
+            var cont1 = new CONTATO()
+            {
+                Tipo = "Email",
+                Contato1 = "luan.fereira@gmail.com",
+                CLIENTE = cli
+            };
+
+            var cont2 = new CONTATO()
+            {
+                Tipo = "Fone",
+                Contato1 = "27380748",
+                CLIENTE = cli
+            };
+
+            ctx.CONTATOS.Add(cont1);
+            ctx.CONTATOS.Add(cont2);
+            ctx.SaveChanges();
+            Console.WriteLine("Registro salvo com sucesso");
+            Console.ReadKey();
         }
     }
 }
