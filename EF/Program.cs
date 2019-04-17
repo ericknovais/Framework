7df5f4a2ed0b785b25f98a2b363a1f43c716c34f
ericknovais <erick.hora@Outlook.com>
2019-04-17 00:25:42 -0300

    Aula 49 - Consultando objetos com Linq

--- EF/Program.cs
+++ EF/Program.cs
@@ -11,6 +11,54 @@ namespace EF
         static void Main(string[] args)
         {
             var ctx = new CadastroEntities();
+            ConsultarClientes3(ctx);
+            Console.ReadKey();
+        }
+
+        private static void ConsultarClientes3(CadastroEntities ctx)
+        {
+            var qry = from _contatos in ctx.CONTATOS
+                      select new
+                      {
+                          Nome = _contatos.CLIENTE.NomeCliente,
+                          Contato = _contatos.Contato1
+                      };
+            foreach (var cli in qry)
+            {
+                Console.WriteLine(cli.Nome + " - " + cli.Contato);
+            }
+        }
+
+        private static void ConsultaClientes2(CadastroEntities ctx)
+        {
+            var qry = from cli in ctx.CLIENTES
+                          //where cli.NomeCliente.Contains("Gi")
+                      orderby cli.IdCliente descending
+                      select cli;
+
+            foreach (var c in qry)
+            {
+                Console.WriteLine(c.IdCliente + " - " + c.NomeCliente + " - " + c.Email);
+            }
+        }
+
+        private static void ConsultarClientes1(CadastroEntities ctx)
+        {
+            foreach (var cli in ctx.CLIENTES)
+            {
+                Console.WriteLine("Nome: " + cli.NomeCliente.ToUpper());
+                foreach (var cont in cli.CONTATOS)
+                {
+                    Console.WriteLine("Tipo: " + cont.Tipo);
+                    Console.WriteLine("Contato: " + cont.Contato1.ToUpper());
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+
+        private static void Inserir(CadastroEntities ctx)
+        {
             var cli = new CLIENTE()
             {
                 NomeCliente = "Luan Ferreira",
