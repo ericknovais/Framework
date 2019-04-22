af7f0810c2115b01243002dcc09cea5942ffb8d7
ericknovais <erick.hora@Outlook.com>
2019-04-21 21:00:11 -0300

    Aula 54 -Criando um cliente Web Api

---
+++ ServicoWebApi/Controllers/ClienteController.cs
@@ -0,0 +1,30 @@
+﻿using ServicoWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServicoWebApi.Controllers
+{
+    public class ClienteController : ApiController
+    {
+        Cliente[] clientes = new Cliente[]
+        {
+            new Cliente{ID =1, Nome="ERICK NOVAIS", Email="erick.hora@Outlook.com" },
+            new Cliente{ID =2, Nome="GIOVANNA MEMOLI", Email="gi.memoli@Outlook.com" }
+        };
+
+        public IEnumerable<Cliente> GetClientes()
+        {
+            return clientes;
+        }
+
+        public IHttpActionResult getCliente(int id)
+        {
+            var cli = clientes.FirstOrDefault((x) => x.ID.Equals(id));
+            return Ok(cli);
+        }
+    }
+}
---
+++ ServicoWebApi/js/script.js
@@ -0,0 +1,26 @@
+﻿var uri = 'api/Cliente';
+
+function formatar(item) {
+    return item.ID + ' - ' + item.Nome + ' - ' + item.Email;
+}
+
+$(document).ready(function () {
+
+    $.getJSON(uri).done(function (data) {
+
+        $.each(data, function (key, item) {
+            $('<li>', { text: formatar(item) }).appendTo($('#clientes'));
+        })
+
+    });
+
+});
+
+function find() {
+
+    var id = $("#ID").val();
+
+    $.getJSON(uri + '/' + id).done(function (data) {
+        $('#cliente').text(formatar(data));
+    })
+}
