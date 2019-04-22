5fd67f4fbd010cba3925c156ca7e21f6ff57e0f6
ericknovais <erick.hora@Outlook.com>
2019-04-22 07:03:20 -0300

    Aula 60 - Interfaces

---
+++ SistemaNotificacao.Interfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaNotificacao.Interfaces
+{
+    //UI
+    class Program
+    {
+        //Associação  concreta
+        //private static Email email = new Email();
+
+        //Associção para Abstração
+        private static INotificacao notificacao;
+
+        static void Main(string[] args)
+        {
+            string Mensagem = "Olá Mundo";
+            notificacao = new Funcionario();
+            notificacao.Enviar(Mensagem); //Delegação
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SistemaNotificacao.Interfaces/framework.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SistemaNotificacao.Interfaces
+{
+    public interface INotificacao
+    {
+        void Enviar(string Mensagem);
+    }
+
+    public abstract class Notificacao : INotificacao
+    {
+        public abstract void Enviar(string Mensagem);
+    }
+
+    public class Email : Notificacao
+    {
+        public override void Enviar(string Mensagem)
+        {
+            Console.WriteLine("Enviando EMAIL: " + Mensagem);
+        }
+    }
+
+    public class SMS : Notificacao
+    {
+        public override void Enviar(string Mensagem)
+        {
+            Console.WriteLine("Enviando SMS: " + Mensagem);
+        }
+    }
+
+    public abstract class Pessoa
+    {
+        public string Nome;
+    }
+
+    public class Funcionario : Pessoa, INotificacao
+    {
+        public void Enviar(string Mensagem)
+        {
+            Console.WriteLine("Funcionario está notificando: " + Mensagem);
+        }
+    }
+}
