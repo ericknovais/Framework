fcdb32e7ffb4bf73b5bb15529a20b51e499b987e
ericknovais <erick.hora@Outlook.com>
2019-04-21 22:46:04 -0300

    Aula 57 - Encapsulamento e Private

---
+++ Sistema.Encapsulamento/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BancoDoBrasil.Contas
+{
+    public enum TipoConta
+    {
+        ContaCorrente,
+        ContaPoupanca
+    };
+
+    public class Conta
+    {
+        // atributos
+        private string Num;
+        private double _saldo;
+        private TipoConta Tipo;
+
+        //construtor
+        public Conta(TipoConta tipo, string num)
+        {
+            this.Num = num;
+            this.Tipo = tipo;
+            this._saldo = 0;
+        }
+
+        //metodos
+        public void Sacar(double valor)
+        {
+            _saldo = _saldo - valor;
+        }
+
+        public void Deposistar(double valor)
+        {
+            _saldo = _saldo + valor;
+        }
+
+        public double getSaldo()
+        {
+            return _saldo;
+        }
+    }
+}
---
+++ Sistema.Encapsulamento/Program.cs
@@ -0,0 +1,19 @@
+﻿using BancoDoBrasil.Contas;
+using System;
+
+namespace Sistema.Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta(TipoConta.ContaCorrente, "0213208");
+
+            conta.Deposistar(500);
+            conta.Sacar(300);
+            
+            Console.WriteLine(conta.getSaldo().ToString());
+            Console.ReadLine();
+        }
+    }
+}
