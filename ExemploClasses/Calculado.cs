084bffb94f1b0cb8f02e83a3934331865fd40d0c
ericknovais <erick.hora@Outlook.com>
2019-04-21 21:30:14 -0300

    Aula 55 - Classes

---
+++ ExemploClasses/Calculado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Erick.Util
+{
+    public class Calculado
+    {
+        public static int Somar(int x, int y)
+        {
+            return x + y;
+            
+        }
+    }
+}
---
+++ ExemploClasses/Program.cs
@@ -0,0 +1,27 @@
+﻿using Erick.Util;
+using System;
+
+namespace ExemploClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número");
+            string xStr = Console.ReadLine(); //UI
+
+            Console.WriteLine("Digite o segundo número");
+            string yStr = Console.ReadLine(); //UI
+
+            int x = Convert.ToInt32(xStr);
+            int y = Convert.ToInt32(yStr);
+
+            int r = Calculado.Somar(x, y);
+
+            Console.WriteLine("O resultado da sua soma é: " + r); //UI
+            Console.ReadKey();
+        }
+
+
+    }
+}
