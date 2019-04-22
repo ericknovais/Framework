4d5f2238be3867aa3e38276b82fc049c7d02d3ff
ericknovais <erick.hora@Outlook.com>
2019-04-21 22:14:06 -0300

    Aula 56 - Herença e Class

---
+++ Exemplo.HerancaClasses/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ENDH.Cadastro
+{
+    public class Cliente
+    {
+        public int Codigo;
+        public string Nome;
+
+        public void Mostrar()
+        {
+            Console.WriteLine("Dados do Cliente: {0} - {1}", Codigo, Nome);
+        }
+    }
+
+    public class PessoaFisica : Cliente
+    {
+        public string CPF;
+
+        public void Mostrar()
+        {
+            base.Mostrar();
+            Console.WriteLine("CPF: {0}", CPF);
+        }
+    }
+
+    public class PessoaJuridica : Cliente
+    {
+        public string CNPJ;
+
+        public void Mostrar()
+        {
+            base.Mostrar();
+            Console.WriteLine("CNPJ: {0}", CNPJ);
+        }
+
+    }
+
+    public class Governo : Cliente
+    {
+        public string Setor;
+    }
+}
---
+++ Exemplo.HerancaClasses/Program.cs
@@ -0,0 +1,30 @@
+﻿using ENDH.Cadastro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo.HerancaClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PessoaFisica cli1 = new PessoaFisica();
+            cli1.Codigo = 1;
+            cli1.Nome = "ERICK";
+            cli1.CPF = "408.926.318-21";
+
+            cli1.Mostrar();
+
+            PessoaJuridica cli2 = new PessoaJuridica();
+            cli2.Codigo = 2;
+            cli2.Nome = "GIOVANNA";
+            cli2.CNPJ = "60.807.435/0001-74";
+
+            cli2.Mostrar();
+            Console.ReadLine();
+        }
+    }
+}
