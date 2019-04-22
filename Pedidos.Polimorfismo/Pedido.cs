2f3ff6ac8f5b58c38c6437847bf0774895b2482d
ericknovais <erick.hora@Outlook.com>
2019-04-22 05:58:13 -0300

    Aula 58 - Polimorfismo e Override

---
+++ Pedidos.Polimorfismo/Pedido.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pedidos.Polimorfismo
+{
+    public abstract class FormaPagamento
+    {
+        public abstract void Pagar(double valor);
+    }
+
+    public class Boleto : FormaPagamento
+    {
+        public override void Pagar(double valor)
+        {
+            Console.WriteLine("Pago valor de {0} no boleto", valor);
+        }
+    }
+
+    public class Cartao : FormaPagamento
+    {
+        public  override void Pagar(double valor)
+        {
+            Console.WriteLine("Pago valor de {0} no cartão", valor);
+        }
+    }
+
+    public class Pedido
+    {
+        public Pedido(FormaPagamento formaPagamento)
+        {
+            this._formaPagamento = formaPagamento;
+        }
+        //Associação, programando para uma abstração
+        private FormaPagamento _formaPagamento;
+
+        public void Pagar(double valor)
+        {
+            //delegação polimorfica
+            this._formaPagamento.Pagar(valor);
+        }
+    }
+}
---
+++ Pedidos.Polimorfismo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pedidos.Polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pedido pedido = new Pedido(new Cartao()); //injeto a dependencia
+            double valor = 2000;
+            pedido.Pagar(valor);
+            Console.ReadLine();
+        }
+    }
+}
