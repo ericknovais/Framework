167819839f9fe760d87d67459d8532d0390621ad
ericknovais <erick.hora@Outlook.com>
2019-04-23 06:10:05 -0300

    Aula 61 - Delegação

---
+++ Delegacao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var valor = 2500;
+            var pedido = new Pedido(new Boleto()); //Injeção de dependencia (DI) / Inversão de Controle (IoC)
+            pedido.Fechar(valor);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Delegacao/framework.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegacao
+{
+    //S.O.L.I.D
+    public class Pedido
+    {
+        //Associação
+        private FormaPagamento _formaPagamento = new Boleto();
+
+        public Pedido(FormaPagamento formaPagamento)
+        {   //guardo internamente a forma de pagamento 
+            //que foi passada no construtor da classe
+            //para que depois seja feita a delegação
+            this._formaPagamento = formaPagamento;
+        }
+
+        public void Fechar(double valor)
+        {
+            //delegar polimórfica
+            //semelhante a um padrão state
+            _formaPagamento.Pagar(valor);
+        }
+    }
+
+    public abstract class FormaPagamento
+    {
+        public abstract void Pagar(double valor);
+    }
+
+    public class Boleto : FormaPagamento
+    {
+        public override void Pagar(double valor)
+        {
+            Console.WriteLine("Pago no boleto no valor de " + valor);
+        }
+    }
+
+    public class Cartao : FormaPagamento
+    {
+        public override void Pagar(double valor)
+        {
+            Console.WriteLine("Pago no cartâo no valor de " + valor);
+        }
+    }
+
+    public class Cheque : FormaPagamento
+    {
+        public override void Pagar(double valor)
+        {
+            Console.WriteLine("Pago no cheque no valor de " + valor);
+        }
+    }
+}
+
