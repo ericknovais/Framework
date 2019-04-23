cb06a71d6613b1f90056b4c4cba3897feaa8591b
ericknovais <erick.hora@Outlook.com>
2019-04-23 07:23:16 -0300

    Aula 64 - Lambda

---
+++ Lambda/Pedido.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    public delegate void PagarNotify(double valor);
+
+    public class Pedido
+    { 
+        //Ponteiro para o método
+        //Forma de injetar código no meu framework
+        //Código da implementação vem de fora
+        public event PagarNotify PagarEvent; //Alguem deve apontar
+
+        public void Pagar(double valor)
+        {
+            //delegação
+            this.PagarEvent(valor);
+        }
+    }
+
+    public class Boleto
+    {
+        public void Pagar(double valor)
+        {
+            Console.WriteLine("Pago boleto no valor: " + valor);
+        }
+    }
+}
---
+++ Lambda/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var valor = 2500;
+            var pedido = new Pedido();
+            //pedido.PagarEvent += new PagarNotify(PagarBoleto);
+            pedido.PagarEvent += (v => Console.WriteLine("Pago no cartão valor de " + v));
+            pedido.Pagar(valor);
+            Console.ReadLine();
+        }
+
+        //public static void PagarBoleto(double valor)
+        //{
+        //    Console.WriteLine("Pago no boleto valor de " + valor);
+        //}
+
+        //public static void PagarCartao(double valor)
+        //{
+        //    Console.WriteLine("Pago no cartão valor de " + valor);
+        //}
+    }
+}
