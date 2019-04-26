45eae356d9f2568cc9f24e4725d270306154ec0b
ericknovais <erick.hora@Outlook.com>
2019-04-25 21:57:02 -0300

    Coneções Genericas

---
+++ ColecoesGenericas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColecoesGenericas
+{
+    class Program
+    {
+        public class Aluno
+        {
+            public int Matricula;
+            public string Nome;
+            public string Curso;
+            public override string ToString()
+            {
+                return Matricula.ToString() + " - " + Nome + " - " + Curso ;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var alunos = new List<Aluno>();
+            alunos.Add(new Aluno() { Matricula = 57, Nome = "Erick Novais", Curso = "Ads" });
+            foreach (var aluno in alunos)
+                Console.WriteLine(aluno);
+            Console.ReadLine();
+        }
+    }
+}
